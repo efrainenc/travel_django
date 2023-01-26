281ed0f87846a07686116040c63e9b39a1d7e306
unknown <efraine387@gmail.com>
2023-01-25 22:02:48 -0500

    search bar working

--- travel_project/travel_app/views.py
+++ travel_project/travel_app/views.py
@@ -15,12 +15,19 @@ class About(TemplateView):
   template_name = 'about.html'
 
 class LocationList(TemplateView):
-  template_name = "location_list.html"
+    template_name = "location_list.html"
 
-  def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    context["locations"] = Location.objects.all()
-    return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # to get the query parameter we have to acccess it in the request.GET dictionary object        
+        country = self.request.GET.get("country")
+        # If a query exists we will filter by name 
+        if country != None:
+            # .filter is the sql WHERE statement and country__icontains is doing a search for any country that contains the query param
+            context["locations"] = Location.objects.filter(country__icontains=country)
+        else:
+            context["locations"] = Location.objects.all()
+        return context
 
 class SpotList(TemplateView):
   template_name = "spot_list.html"
